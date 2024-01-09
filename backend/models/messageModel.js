ca688f660fced4d8cda482149bd400b28a3d1833
RahulSahu760 <rahulsahu1021@gmail.com>
2024-01-09 21:48:06 +0530

    added files

---
+++ backend/models/messageModel.js
@@ -0,0 +1,16 @@
+//the name or id of the sender, content of the message, the ref of the chat to which it belongs to
+const mongoose = require("mongoose");
+
+const messageModel = mongoose.Schema(
+  {
+    sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    content: { type: String, trim: true },
+    chat: { type: mongoose.Schema.Types.ObjectId, ref: "Chat" },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Message = mongoose.model("Message", messageModel);
+module.exports = Message;
---
+++ backend/server.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+
+dotenv.config();
+connectDB();
+const app = express();
+const corsOptions = {
+  origin: "http://localhost:3000", // Replace with the domain you want to allow
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
+app.use(express.json()); //to accept json data
+
+app.get("/", (req, res) => {
+  res.send("API is running successfully.Test 1");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, console.log(`Server started on port ${PORT}`));
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    var chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("chat.users not defined");
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+  socket.off("setup", () => {
+    console.log("USER DISCONNECTED");
+    socket.leave(userData._id);
+  });
+});
